2bde574d2676134775d3c19f5bade5c95317848b
rizkyawali <rizkyawalli@gmail.com>
2021-06-03 12:52:52 +0700

    Add Title Icon

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" href="{{ url('assets/frontend/images/logo-nav.png') }}" type="image/x-icon">
     <title>Jayagiri - @yield('title')</title>
 
     @stack('prepend-style')
